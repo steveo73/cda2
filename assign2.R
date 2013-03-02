6c3b0c21237b57b29b958d9a35ae893c248d3174
steveo73 <oneill73@tpg.com.au>
2013-03-02 21:35:29 +1100

    Fixed a couple of data errors

--- assign2.R
+++ assign2.R
@@ -10,18 +10,24 @@ download.file(fileUrl,fileLocal,method="curl")
 # 4.Load Data to R
 dateDownloaded <- date()
 load(fileLocal)
-# 5 Get unique number of subjects within data-set and view the unique values
+# 5. Complete validation tests on the data
+# (i) Get unique number of subjects within data-set and view the unique values
 length(unique(samsungData$subject))
 unique(samsungData$subject)
-# 6 Get unique number of activities to predict in the data-set and view the unique values
+# (ii) Get unique number of activities to predict in the data-set and view the unique values
 length(unique(samsungData$activity))
 unique(samsungData$activity)
-# 7 complete some data validation checks
-
-
-# Sub-set the data into training, validation and testing data-sets
+# (iii) test for duplicate names within the data-set
+names(samsungData)[duplicated(names(samsungData))]
+# Duplicate names exist so we have to amend the data - pushing the same data to a data frame object removes the duplicate names
+samsungData <- data.frame(samsungData)
+# 6. Sub-set the data into training, validation and testing data-sets
 trainData <- samsungData [samsungData$subject < 10,]
 validationData <-samsungData [samsungData$subject %in% c(10:20),]
 testData <- samsungData [samsungData$subject %in% c(27:30),]
-
+# 7. Split the training set into 2 data sets in order to pick
+# (i) features
+# (ii) prediction function
+trainData1 <- trainData [trainData$subject < 6,]
+trainData2 <- trainData [trainData$subject > 5,]
 
